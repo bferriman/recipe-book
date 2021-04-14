8f604393b4ffa5658084511ce9265d739557a342
Ben F <56423163+bferriman@users.noreply.github.com>
2021-04-14 14:26:36 -0600

    added icons to bottom tab navigator

--- navigation/MealsNavigator.js
+++ navigation/MealsNavigator.js
@@ -1,8 +1,10 @@
+import React from "react";
 import { Platform } from "react-native";
 import { createAppContainer } from "react-navigation";
 import { createStackNavigator } from "react-navigation-stack";
 import { createBottomTabNavigator } from "react-navigation-tabs";
 import { createDrawerNavigator} from "react-navigation-drawer";
+import { Ionicons } from "@expo/vector-icons";
 
 import CategoriesScreen from "../screens/CategoriesScreen";
 import CategoryMealsScreen from "../screens/CategoryMealsScreen";
@@ -26,8 +28,26 @@ const MealsNavigator = createStackNavigator({
 });
 
 const MealsFavTabNavigator = createBottomTabNavigator({
-  Meals: MealsNavigator,
-  Favorites: FavoritesScreen
+  Meals: {
+    screen: MealsNavigator,
+    navigationOptions: {
+      tabBarIcon: (tabInfo) => {
+        return <Ionicons name="ios-restaurant" size={25} color={tabInfo.tintColor} />;
+      }
+    }
+  },
+  Favorites: {
+    screen: FavoritesScreen,
+    navigationOptions: {
+      tabBarIcon: (tabInfo) => {
+        return <Ionicons name="ios-star" size={25} color={tabInfo.tintColor} />;
+      }
+    }
+  }
+}, {
+  tabBarOptions: {
+    activeTintColor: Colors.accentColor
+  }
 });
 
 export default createAppContainer(MealsFavTabNavigator);
