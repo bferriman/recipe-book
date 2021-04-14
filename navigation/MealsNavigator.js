ff5dedd54700c9afef75db396efd60410a6c84f9
Ben F <56423163+bferriman@users.noreply.github.com>
2021-04-14 15:10:24 -0600

    added color styling to bottom tabs

--- navigation/MealsNavigator.js
+++ navigation/MealsNavigator.js
@@ -34,7 +34,8 @@ const tabScreenConfig = {
     navigationOptions: {
       tabBarIcon: (tabInfo) => {
         return <Ionicons name="ios-restaurant" size={25} color={tabInfo.tintColor} />;
-      }
+      },
+      tabBarColor: Colors.primaryColor
     }
   },
   Favorites: {
@@ -42,7 +43,8 @@ const tabScreenConfig = {
     navigationOptions: {
       tabBarIcon: (tabInfo) => {
         return <Ionicons name="ios-star" size={25} color={tabInfo.tintColor} />;
-      }
+      },
+      tabBarColor: Colors.accentColor
     }
   }
 }
@@ -50,7 +52,7 @@ const tabScreenConfig = {
 const MealsFavTabNavigator = 
   Platform.OS === "android" 
     ? createMaterialBottomTabNavigator(tabScreenConfig, {
-      activeColor: Colors.accentColor,
+      activeColor: "white",
       shifting: true
     })
     : createBottomTabNavigator(tabScreenConfig, {
