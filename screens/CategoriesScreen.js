12df697b62e0bcd3ce08ba019ca24335eaace974
Ben F <56423163+bferriman@users.noreply.github.com>
2021-04-15 23:23:13 -0600

    added header hamburger nav button

--- screens/CategoriesScreen.js
+++ screens/CategoriesScreen.js
@@ -1,7 +1,9 @@
 import React from "react";
 import { View, Text, StyleSheet, FlatList, TouchableOpacity } from "react-native";
+import { HeaderButtons, Item } from "react-navigation-header-buttons";
 
 import { CATEGORIES } from "../data/dummy-data";
+import HeaderButton from "../components/HeaderButton";
 import CategoryGridTile from "../components/CategoryGridTile";
 
 const CategoriesScreen = props => {
@@ -28,8 +30,20 @@ const CategoriesScreen = props => {
   );
 };
 
-CategoriesScreen.navigationOptions = {
-  headerTitle: "Meal Categories"
+CategoriesScreen.navigationOptions = navData => {
+  return {
+    headerTitle: "Meal Categories",
+    headerLeft: () => 
+      <HeaderButtons HeaderButtonComponent={HeaderButton}>
+        <Item 
+          title="Menu" 
+          iconName="ios-menu" 
+          onPress={() => {
+            navData.navigation.toggleDrawer();
+          }}
+        />
+      </HeaderButtons>
+  };
 };
 
 const styles = StyleSheet.create({
