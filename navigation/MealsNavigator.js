b269b9a7fc4545954a8007bbe8304ca175de4c5e
Ben F <56423163+bferriman@users.noreply.github.com>
2021-04-19 15:06:16 -0600

    made some styling and labeling improvements to drawer nav

--- navigation/MealsNavigator.js
+++ navigation/MealsNavigator.js
@@ -78,10 +78,20 @@ const FiltersNavigator = createStackNavigator({
 });
 
 const MainNavigator = createDrawerNavigator({
-  MealsFavs: MealsFavTabNavigator,
+  MealsFavs: {
+    screen: MealsFavTabNavigator,
+    navigationOptions: {
+      drawerLabel: "Meals"
+    }
+  },
   Filters: FiltersNavigator
-}
-
-);
+}, {
+  contentOptions: {
+    activeTintColor: Colors.accentColor,
+    labelStyle: {
+      fontFamily: "open-sans-bold"
+    }
+  }
+});
 
 export default createAppContainer(MainNavigator);
