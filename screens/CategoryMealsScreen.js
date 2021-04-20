b5d7baaa9dc5868f0d4a9f7a8f2dfb1aae283382
Ben F <56423163+bferriman@users.noreply.github.com>
2021-04-20 12:45:02 -0600

    replaced import of dummy data with connection to redux state data

--- screens/CategoryMealsScreen.js
+++ screens/CategoryMealsScreen.js
@@ -1,6 +1,6 @@
 import React from "react";
-
-import { CATEGORIES, MEALS } from "../data/dummy-data";
+import { useSelector } from "react-redux";
+import { CATEGORIES } from "../data/dummy-data";
 
 import MealList from "../components/MealList";
 
@@ -8,7 +8,9 @@ const CategoryMealScreen = props => {
 
   const catId = props.navigation.getParam("categoryId");
 
-  const displayedMeals = MEALS.filter(meal => meal.categoryIds.indexOf(catId) >= 0);
+  const availableMeals = useSelector(state => state.meals.filteredMeals);
+
+  const displayedMeals = availableMeals.filter(meal => meal.categoryIds.indexOf(catId) >= 0);
 
   return (
     <MealList listData={displayedMeals} navigation={props.navigation} />
