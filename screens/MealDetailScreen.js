eca4658e31ff6263f83d45ff059c69e67d7b6c8b
Ben F <56423163+bferriman@users.noreply.github.com>
2021-04-20 14:01:22 -0600

    configured to display different icons depending on whether the meal is a Favorite

--- screens/MealDetailScreen.js
+++ screens/MealDetailScreen.js
@@ -17,6 +17,7 @@ const MealDetailScreen = props => {
 
   const allMeals = useSelector(state => state.meals.meals);
   const mealId = props.navigation.getParam("mealId");
+  const mealIsFavorite = useSelector(state => state.meals.favoriteMeals.some(meal => meal.id === mealId));
 
   const selectedMeal = allMeals.find(meal => meal.id === mealId);
 
@@ -29,6 +30,10 @@ const MealDetailScreen = props => {
   useEffect(() => {
     props.navigation.setParams({toggleFav: toggleFavoriteHandler});
   }, [toggleFavoriteHandler]);
+
+  useEffect(() => {
+    props.navigation.setParams({isFav: mealIsFavorite});
+  }, [mealIsFavorite]);
 
   return (
     <ScrollView>
@@ -53,13 +58,14 @@ const MealDetailScreen = props => {
 MealDetailScreen.navigationOptions = (navigationData) => {
   const mealTitle = navigationData.navigation.getParam("mealTitle");
   const toggleFavorite = navigationData.navigation.getParam("toggleFav");
+  const isFavorite = navigationData.navigation.getParam("isFav");
 
   return {
     headerTitle: mealTitle,
     headerRight: () => <HeaderButtons HeaderButtonComponent={HeaderButton}>
       <Item 
         title="Favorite" 
-        iconName="ios-star" 
+        iconName={isFavorite ? "ios-star" : "ios-star-outline"} 
         onPress={toggleFavorite}
       />
     </HeaderButtons>
