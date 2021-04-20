146d077b346a7cdec7d9a9fb639a493aacfd0a59
Ben F <56423163+bferriman@users.noreply.github.com>
2021-04-20 13:24:43 -0600

    added logic to handle the TOGGLE_FAVORITE action

--- store/reducers/meals.js
+++ store/reducers/meals.js
@@ -1,4 +1,5 @@
 import { MEALS } from "../../data/dummy-data";
+import { TOGGLE_FAVORITE } from "../actions/meals";
 
 const initialState = {
   meals: MEALS,
@@ -7,7 +8,21 @@ const initialState = {
 };
 
 const mealsReducer = (state = initialState, action) => {
-  return state;
+  switch (action.type) {
+    case TOGGLE_FAVORITE:
+      //determine whether the toggled meal is in the favoriteMeals array
+      const existingIndex = state.favoriteMeals.findIndex(meal => meal.id === action.mealId);
+      if (existingIndex >= 0) {  //meal is currently a favorite, need to remove it
+        const updatedFavMeals = [...state.favoriteMeals];
+        updatedFavMeals.splice(existingIndex, 1);
+        return { ...state, favoriteMeals: updatedFavMeals };
+      } else {  //meal is not currently a favorite, need to add it
+        const newMeal = state.meals.find(meal => meal.id === action.mealId);
+        return { ...state, favoriteMeals: state.favoriteMeals.concat(newMeal) };
+      }
+    default:
+      return state;
+  }
 };
 
 export default mealsReducer;
