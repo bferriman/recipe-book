e3809cb4f85979165fcbdc1671315758392e9eb1
Ben F <56423163+bferriman@users.noreply.github.com>
2021-04-20 18:34:32 -0600

    added placeholder text to display when user has no favorites

--- screens/FavoritesScreen.js
+++ screens/FavoritesScreen.js
@@ -1,13 +1,21 @@
 import React from "react";
+import { View, StyleSheet } from "react-native";
 import { HeaderButtons, Item } from "react-navigation-header-buttons";
 
 import MealList from "../components/MealList";
 import HeaderButton from "../components/HeaderButton";
+import DefaultText from "../components/DefaultText";
 import { useSelector } from "react-redux";
 
 const FavoritesScreen = props => {
 
   const favMeals = useSelector(state => state.meals.favoriteMeals);
+
+  if (favMeals.length === 0 || !favMeals) {
+    return <View style={styles.content}>
+      <DefaultText>Your Favorites List is Empty!</DefaultText>
+    </View>
+  }
 
   return (
     <MealList listData={favMeals} navigation={props.navigation} />
@@ -29,5 +37,13 @@ FavoritesScreen.navigationOptions = navData => {
     </HeaderButtons>
   };
 };
+
+const styles = StyleSheet.create({
+  content: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center"
+  }
+});
 
 export default FavoritesScreen;
