9e529ef575f0203b44adea0d44732ae65ceb0c72
Ben F <56423163+bferriman@users.noreply.github.com>
2021-04-20 21:14:19 -0600

    added fallback text to display when there are no filtered meals to show

--- screens/CategoryMealsScreen.js
+++ screens/CategoryMealsScreen.js
@@ -1,8 +1,10 @@
 import React from "react";
+import { View, StyleSheet } from "react-native";
 import { useSelector } from "react-redux";
 import { CATEGORIES } from "../data/dummy-data";
 
 import MealList from "../components/MealList";
+import DefaultText from "../components/DefaultText";
 
 const CategoryMealScreen = props => {
 
@@ -11,6 +13,12 @@ const CategoryMealScreen = props => {
   const availableMeals = useSelector(state => state.meals.filteredMeals);
 
   const displayedMeals = availableMeals.filter(meal => meal.categoryIds.indexOf(catId) >= 0);
+
+  if (displayedMeals.length === 0) {
+    return <View style={styles.content}>
+      <DefaultText>No meals found. Check your Filters?</DefaultText>
+    </View>
+  } 
 
   return (
     <MealList listData={displayedMeals} navigation={props.navigation} />
@@ -25,5 +33,13 @@ CategoryMealScreen.navigationOptions = (navigationData) => {
     headerTitle: selectedCategory.title  
   };
 };
+
+const styles = StyleSheet.create({
+  content: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center"
+  }
+});
 
 export default CategoryMealScreen;
