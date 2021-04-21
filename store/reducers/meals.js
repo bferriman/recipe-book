881b9c8b773db6d032b95b28fa47a8bbc56fa7cb
Ben F <56423163+bferriman@users.noreply.github.com>
2021-04-20 20:55:40 -0600

    coded reducer for SET_FILTERS action

--- store/reducers/meals.js
+++ store/reducers/meals.js
@@ -1,5 +1,5 @@
 import { MEALS } from "../../data/dummy-data";
-import { TOGGLE_FAVORITE } from "../actions/meals";
+import { TOGGLE_FAVORITE, SET_FILTERS } from "../actions/meals";
 
 const initialState = {
   meals: MEALS,
@@ -9,6 +9,7 @@ const initialState = {
 
 const mealsReducer = (state = initialState, action) => {
   switch (action.type) {
+    
     case TOGGLE_FAVORITE:
       //determine whether the toggled meal is in the favoriteMeals array
       const existingIndex = state.favoriteMeals.findIndex(meal => meal.id === action.mealId);
@@ -20,6 +21,26 @@ const mealsReducer = (state = initialState, action) => {
         const newMeal = state.meals.find(meal => meal.id === action.mealId);
         return { ...state, favoriteMeals: state.favoriteMeals.concat(newMeal) };
       }
+
+    case SET_FILTERS:
+      const appliedFilters = actions.filters;
+      const newFilteredMeals = state.meals.filter(meal => {
+        if (appliedFilters.glutenFree && !meal.isGlutenFree) {
+          return false;
+        }
+        if (appliedFilters.lactoseFree && !meal.isLactoseFree) {
+          return false;
+        }
+        if (appliedFilters.vegan && !meal.isVegan) {
+          return false;
+        }
+        if (appliedFilters.vegetarian && !meal.isVegetarian) {
+          return false;
+        }
+        return true;
+      });
+      return { ...state, filteredMeals: newFilteredMeals };
+
     default:
       return state;
   }
